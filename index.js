09a9a76ca3bcea7cbfeaceb96ca8b8de528192f5
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2024-01-25 11:15:18 +0100

    p2p

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 const express = require("express");
-const { createServer } = require("node:http");
-const { join } = require("node:path");
-const { Server } = require("socket.io");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const rooms = {};
 
 app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  console.log("A user connected");
+
+  socket.on("join-room", (room) => {
+    if (!rooms[room]) {
+      rooms[room] = { users: {} };
+    }
+
+    rooms[room].users[socket.id] = socket.id;
+    socket.join(room);
+
+    // Broadcast to all users in the room when a new user joins
+    io.to(room).emit("user-connected", socket.id);
+
+    socket.on("disconnect", () => {
+      // Remove the user when they disconnect
+      delete rooms[room].users[socket.id];
+
+      // Broadcast to all users in the room when a user disconnects
+      io.to(room).emit("user-disconnected", socket.id);
+    });
+
+    socket.on("offer", (offer, targetSocketId) => {
+      io.to(targetSocketId).emit("offer", offer, socket.id);
+    });
+
+    socket.on("answer", (answer, targetSocketId) => {
+      io.to(targetSocketId).emit("answer", answer);
+    });
+
+    socket.on("ice-candidate", (candidate, targetSocketId) => {
+      io.to(targetSocketId).emit("ice-candidate", candidate);
+    });
   });
 });
 
 server.listen(3000, () => {
-  console.log("server running at http://localhost:3000");
+  console.log("Server is running on http://localhost:3000");
 });
